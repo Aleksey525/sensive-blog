60d06db3ff4f74cc8ab538673c5a29c9f69ebe10
Aleksey525 <alex_tolchin@mail.ru>
2024-05-04 13:32:41 +0300

    Revert "replace sorted func with order_by"
    This reverts commit b990924151b5361a6dee4571317d6da40de48b54.

--- blog/views.py
+++ blog/views.py
@@ -29,8 +29,9 @@ def serialize_tag(tag):
 
 
 def index(request):
-    posts = Post.objects.annotate(Count('likes')).order_by('-likes__count')
-    most_popular_posts = list(posts)[:5]
+    posts = Post.objects.annotate(Count('likes'))
+    popular_posts = sorted(posts, key=get_likes_count)
+    most_popular_posts = popular_posts[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -118,3 +119,7 @@ def contacts(request):
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
     return render(request, 'contacts.html', {})
+
+
+def get_likes_count(post):
+    return post.likes__count
